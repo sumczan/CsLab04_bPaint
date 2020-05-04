9fbc0ad546fa1423b8926e67ea1f08ac62792c26
sumczan <45795394+sumczan@users.noreply.github.com>
2020-05-04 19:25:40 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace $safeprojectname$
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripComboBox2 = new System.Windows.Forms.ToolStripComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1,
+            this.toolStripComboBox2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Items.AddRange(new object[] {
+            "Pen",
+            "Rectangle",
+            "Line"});
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 25);
+            this.toolStripComboBox1.Click += new System.EventHandler(this.toolStripComboBox1_Click);
+            // 
+            // toolStripComboBox2
+            // 
+            this.toolStripComboBox2.Items.AddRange(new object[] {
+            "Black",
+            "Red",
+            "Green",
+            "Blue",
+            "Yellow"});
+            this.toolStripComboBox2.Name = "toolStripComboBox2";
+            this.toolStripComboBox2.Size = new System.Drawing.Size(121, 25);
+            this.toolStripComboBox2.Click += new System.EventHandler(this.toolStripComboBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 423);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace $safeprojectname$
+{
+    public partial class Form1 : Form
+    {
+        private enum Shape
+        {
+            Pen,
+            Rectangle,
+            Line
+        }
+
+        private Shape currentShape;
+        private Color currentColor;
+
+        Point start;
+        Point end;
+
+        Graphics drawing;
+
+        Pen pen;
+
+        float penThickness;
+
+        public Form1()
+        {
+            InitializeComponent();
+            toolStripComboBox1.SelectedIndex = 0;
+            toolStripComboBox2.SelectedIndex = 0;
+            drawing = pictureBox1.CreateGraphics();
+        }
+
+        private void toolStripComboBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripComboBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            currentShape = (Shape)Enum.Parse(typeof(Shape), (String)toolStripComboBox1.SelectedItem);
+            //currentShape = (Shape)toolStripComboBox1.SelectedItem;
+            //currentColor = (Color)toolStripComboBox2.SelectedItem;
+            currentColor = Color.FromName((String)toolStripComboBox2.SelectedItem);
+            start.X = e.X;
+            start.Y = e.Y;
+
+            Brush brush = new SolidBrush(currentColor);
+            pen = new Pen(brush);
+            penThickness = 2.0f;
+            pen.Width = penThickness;
+
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                end.X = e.X;
+                end.Y = e.Y;
+                if(currentShape == Shape.Pen)
+                {
+                    DrawPen();
+                    start.X = e.X;
+                    start.Y = e.Y;
+                }
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            switch (currentShape)
+            {
+                case Shape.Pen:
+                    DrawPen();
+                    break;
+                case Shape.Rectangle:
+                    DrawRectangle();
+                    break;
+                case Shape.Line:
+                    DrawLine();
+                    break;
+                default:
+                    break;
+            }
+                
+        }
+
+        private void DrawPen()
+        {
+            drawing.SmoothingMode = SmoothingMode.AntiAlias;
+            drawing.DrawLine(pen, start, end);
+        }
+
+        private void DrawRectangle()
+        {
+            drawing.SmoothingMode = SmoothingMode.AntiAlias;
+            int x;
+            int y;
+            int width;
+            int height;
+
+            x = ((start.X < end.X) ? start.X : end.X);
+            y = ((start.Y < end.Y) ? start.Y : end.Y);
+            width = Math.Abs(start.X - end.X);
+            height = Math.Abs(start.Y - end.Y);
+
+            drawing.DrawRectangle(pen, x, y, width, height);
+        }
+
+        private void DrawLine()
+        {
+            drawing.SmoothingMode = SmoothingMode.AntiAlias;
+            drawing.DrawLine(pen, start, end);
+        }
+
+
+    }
+}
